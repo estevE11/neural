d6d5d4127137cbe326d9c542fab521e1d363736c
Roger Esteve <rogeresteve30@gmail.com>
2023-03-10 13:43:34 +0100

    api to get model prediction

---
+++ bg/server.py
@@ -0,0 +1,38 @@
+import torch
+import numpy as np
+from model import BG
+
+from flask import Flask, Response, request, jsonify
+
+app = Flask(__name__)
+
+handler = None
+
+@app.route('/',methods=['GET'])
+def getQuestion():
+    r = int(request.args.get("r"))
+    g = int(request.args.get("g"))
+    b = int(request.args.get("b"))
+    out = 1 if handler.guess(r, g, b)[0] < 0.5 else 0
+    print(out)
+    response=jsonify({"data": "buenas"})
+    
+    return response
+
+class AIHandler():
+    def __init__(self):
+        self.model = BG()
+        self.model.load_state_dict(torch.load("models/color_2.pt"))
+        self.model.eval()
+
+    def guess(self, r, g, b):
+        color = torch.tensor(np.array([r/255, g/255, b/255])).float()
+        g = self.model(color)
+        return g
+
+if __name__ == "__main__":
+    handler = AIHandler()
+    app.run()
+
+
+    
